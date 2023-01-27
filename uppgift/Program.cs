b94e0d6d79ab1df8fb037e2077fd85dfa5d847c5
Elev <Elev@TEB125111>
2023-01-27 13:59:12 +0100

    Add project files.

---
+++ uppgift/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] S = { "Ja", "Nej", "Kanske", "Fråga mig igen", "Om du har tur", "Aldrig", "Tveksam", "Kanske inte","väldigt tveksamt", "Kan inte förutsäga nu"};
+Random Sl = new Random();
+
+string F = " ";
+
+while (F != "a")    {
+    Console.WriteLine("Skriv en fråga till Magic 8-ball");
+    F = Console.ReadLine();
+
+    if (F == "a") {
+        break;
+    }
+    Console.WriteLine(S[Sl.Next(S.Length)]);
+    Console.WriteLine();
+}
